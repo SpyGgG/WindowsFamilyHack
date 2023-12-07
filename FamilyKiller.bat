3e28430b270035a513620455fe3fb9e434ebb81b
SpyGgG <131914767+SpyGgG@users.noreply.github.com>
2023-12-07 18:27:16 +0100

    Update Killer.bat

--- FamilyKiller.bat
+++ FamilyKiller.bat
@@ -1,4 +1,20 @@
 @ECHO OFF
+:: Check for admin rights
+NET FILE 1>NUL 2>NUL
+if "%ERRORLEVEL%" NEQ "0" (
+    ECHO Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto :main )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:main
+:: Your code here
 :loop
 tasklist /FI "IMAGENAME eq WpcMon.exe" 2>NUL | find /I /N "WpcMon.exe">NUL
 IF "%ERRORLEVEL%"=="0" (
